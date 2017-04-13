0e317e394364fa2d8222385aaffa520d229c6d46
Derry Berni Cahyady <derry@detik.com>
2017-04-13 14:20:02 +0700

    new function

---
+++ library.php
@@ -0,0 +1,15 @@
+<?php
+/* Sort Array Descending */
+usort($output, function($a, $b){
+  return strtotime($b['date']) - strtotime($a['date']);
+});
+
+/* Sort Array Ascending */
+usort($output, function($a, $b){
+  return strtotime($a['date']) - strtotime($b['date']);
+});
+
+
+/* Bagi data 1-10 */
+$listVideo  = array_slice($getVideo, 0, 10);
+?>
